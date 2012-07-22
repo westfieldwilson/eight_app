03ef683e40021fb954ae4f5b07512cd38295406e
westfieldwilson <westfieldwilson@gmail.com>
2012-07-22 00:17:33 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EightApp::Application.config.secret_token = 'fde1189165a0481407d0bd49b2859782f382f33d29e4cbb9d68bac1469064202fd4f7c0d956acf6823e96fe159873f754ce35bd4082e5188102a519f1182ec2e'
